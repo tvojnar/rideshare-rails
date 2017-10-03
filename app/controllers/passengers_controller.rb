181c7d9a41e43273f174e4d1cb2cdc84df4185d3
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-03 15:10:05 -0700

    Add _form. Add new and create functionality for passengers

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -7,16 +7,27 @@ class PassengersController < ApplicationController
     @passenger = Passenger.find(params[:id])
   end
 
+  def new
+    @passenger = Passenger.new
+  end
 # TODO: add these methods in when I add create functionality
-  # def create
-  #   strong_params = passenger_params
-  #   @passenger = Passenger.new(strong_params)
-  # end
-  #
-  # private
-  #   def passenger_params
-  #     return params.require(:passenger).permit(:name, :phone_number)
-  #   end
+  def create
+    strong_params = passenger_params
+    @passenger = Passenger.new(strong_params)
 
-  # TODO: make edit use strong params too! 
+    if @passenger.save
+      # if the save was sucessful then:
+      redirect_to passengers_path
+    else
+      # if @passenger wasn't saved to the database then:
+       render :new
+    end # if/else
+  end # create
+
+  private
+    def passenger_params
+      return params.require(:passenger).permit(:name, :phone_number)
+    end
+
+  # TODO: make edit use strong params too!
 end
