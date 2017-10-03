f5baa17e43948c3085d7c0c7fc54201075a1968c
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-03 13:18:26 -0700

    Add strong params comments to passenger controller

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -6,4 +6,17 @@ class PassengersController < ApplicationController
   def show
     @passenger = Passenger.find(params[:id])
   end
+
+# TODO: add these methods in when I add create functionality
+  # def create
+  #   strong_params = passenger_params
+  #   @passenger = Passenger.new(strong_params)
+  # end
+  #
+  # private
+  #   def passenger_params
+  #     return params.require(:passenger).permit(:name, :phone_number)
+  #   end
+
+  # TODO: make edit use strong params too! 
 end
