e929596716ebd41cf659d6416f2f619a740d4100
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-03 21:31:17 -0700

    Add total_cost method to passenger.rb. Changed _all_trips.html.erb to display the cost in dollars for each trip

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,13 @@ class Passenger < ApplicationRecord
   # TODO: Add custom validation error messages
   validates :name, presence: true
   validates :phone_number, presence: true
+
+  def total_cost
+    # TODO: can I use the inject method here instead?
+    total = 0
+    trips.each do |trip|
+      total += trip.cost
+    end # .each
+    return (total / 100).round(2)
+  end
 end
