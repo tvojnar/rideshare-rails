f0abc4afe3ac985bef0119811c89a74c0e45e0a0
Roxanne <argonkryptonneon@gmail.com>
2017-10-04 12:00:33 -0700

    added ratings and earnings methods

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,25 @@
 class Driver < ApplicationRecord
-  has_many :trips 
+  has_many :trips
+  validates :name, presence: true
+  validates :vin, presence: true
+
+  def total_cost
+    # TODO: can I use the inject method here instead?
+    total_of_trips = []
+    trips.each do |trip|
+      total_of_trips << trip.cost
+    end
+    total = ((total_of_trips.sum * (1 - 0.15)) / total_of_trips.length).round(2)
+  end
+
+  def rating
+    total_ratings = 0
+    count = 0
+    trips.each do |trip|
+      total_ratings += trip.rating
+      count += 1
+    end
+    average = total_ratings/count
+  end
+
 end
