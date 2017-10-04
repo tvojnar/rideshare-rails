ab60a751c300704f17fce4cec376a2786331d209
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-04 15:17:27 -0700

    Add edit page for trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,4 +6,8 @@ class TripsController < ApplicationController
   def show
     @trip = Trip.find(params[:id])
   end # show
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end # edit
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,9 @@
 class Trip < ApplicationRecord
   belongs_to :driver
-  belongs_to :passenger 
+  belongs_to :passenger
+
+  # def cost_in_dollars
+  #   trip = trips.find(params[:id])
+  #   return (trip.cost / 100).round(2)
+  # end # cost_in_dollars
 end
