df980733178cf692a9ec233f69ba5a3132838d84
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-04 21:44:56 -0700

    Added nested routes to show the index page for trips by a passenger and to create a new trip for a passenger whith links to these features on the show page for passenger

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,50 +1,57 @@
 class TripsController < ApplicationController
-  # comment 
   def index
-    @trips = Trip.all
+    if params[:passenger_id]
+      passenger = Passenger.find_by(id: params[:passenger_id])
+      @trips = passenger.trips
+    else
+      @trips = Trip.all
+    end # if/else
+  end # index
+
+    def show
+      @trip = Trip.find(params[:id])
+    end # show
+
+    def new
+      @trip = Trip.new
+      if params[:passenger_id]
+        @trip.passenger_id = params[:passenger_id]
+      end
+    end # new
+
+    def create
+      @trip = Trip.new(trip_params)
+      @trip.cost = @trip.remove_decimal_from_cost
+
+      if @trip.save
+        # TODO: Should I change this to a nester rout?
+        redirect_to passenger_path(@trip.passenger_id)
+      else
+        render :new
+      end # if/else
+    end # create
+
+    def edit
+      @trip = Trip.find(params[:id])
+    end # edit
+
+    def update
+      @trip = Trip.find(params[:id])
+      @trip.update_attributes(trip_params)
+      # removes the decimal that the user entered in the form to update the cost
+      @trip.cost = @trip.remove_decimal_from_cost
+
+      if @trip.save
+        # TODO: Should I change this to a nester rout?
+        redirect_to passenger_path(@trip.passenger_id)
+      else
+        render :edit
+      end # if/else
+    end # update
+
+    private
+    def trip_params
+      return params.require(:trip).permit(:driver_id, :passenger_id, :date, :rating, :cost)
+    end # trip_params
+
   end
-
-  def show
-    @trip = Trip.find(params[:id])
-  end # show
-
-  def new
-    @trip = Trip.new
-  end # new
-
-  def create
-    @trip = Trip.new(trip_params)
-    @trip.cost = @trip.remove_decimal_from_cost
-
-    if @trip.save
-      # TODO: Should I change this to a nester rout?
-      redirect_to passenger_path(@trip.passenger_id)
-    else
-      render :new
-    end # if/else
-  end # create
-
-  def edit
-    @trip = Trip.find(params[:id])
-  end # edit
-
-  def update
-    @trip = Trip.find(params[:id])
-    @trip.update_attributes(trip_params)
-    # removes the decimal that the user entered in the form to update the cost
-    @trip.cost = @trip.remove_decimal_from_cost
-
-    if @trip.save
-      # TODO: Should I change this to a nester rout?
-      redirect_to passenger_path(@trip.passenger_id)
-    else
-      render :edit
-    end # if/else
-  end # update
-
-private
-def trip_params
-  return params.require(:trip).permit(:driver_id, :passenger_id, :date, :rating, :cost)
-end # trip_params
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   resources :passengers
+   resources :passengers do
+     # TODO: can I use only here or will we need all the routes?
+     resources :trips
+   end
+
    resources :trips
    resources :drivers
    root 'main#index'
