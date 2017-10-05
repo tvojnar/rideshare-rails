87ae5b1ad04d641d17f42ba66eca53777c49afe5
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-05 14:24:38 -0700

    Remove passenger validation and duplicate cost validation from trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,13 +3,13 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
 
-  validates_presence_of :passenger
+
   validates_presence_of :driver_id
   validates_presence_of :passenger_id
   validates_presence_of :date
   validates_presence_of :cost
   validates_inclusion_of :rating, :in => 0..5
-  validates_presence_of  :cost
+
 
   # TODO: make this method work later!
   # def cost_in_dollars
